2a8ad288c24f00cfe8c68e3c5ec5470cc42b83a8
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-03-06 10:58:10 +0100

    Webpack + Vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+import Vue from 'vue';
+import axios from 'axios';
+
+Vue.prototype.$http = axios;
+
+const app = new Vue({
+  el: '#container',
+  delimiters: ['v{', '}'],
+  data() {
+    return {
+      title: 'Creham'
+    }
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+
+var config = {
+  entry: './assets/js/main.js',
+  output: {
+    path: path.resolve(__dirname, './assets/js'),
+    filename: 'build.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: ['vue-style-loader', 'css-loader', 'sass-loader']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.mode = 'production'
+}
+
+module.exports = config;
