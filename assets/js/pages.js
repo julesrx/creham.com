dc4f3e76611c7bfc08f7b8174b335e875709019f
julesrx <me@julesrx.fr>
2020-01-05 19:44:25 +0100

    highlightText

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -3,6 +3,8 @@ var mask = slider.querySelector('.mask');
 
 var imgs = mask.querySelectorAll('img.slider-img');
 
+highlightText(0);
+
 setInterval(function () {
   imgs = mask.querySelectorAll('img.slider-img');
 
@@ -17,5 +19,19 @@ setInterval(function () {
 
     current.classList.remove('moving');
     after.classList.remove('moving');
-  }, 1000);
+
+    var pos = Number(after.attributes['src'].value.match(/(?<=\/)\d(?=-)/g)[0]);
+    console.log(after.attributes['src'].value);
+    highlightText(pos - 1);
+  }, 500);
 }, 3000);
+
+function highlightText(pos) {
+  var texts = document.getElementsByClassName('slider-text')[0].querySelectorAll('span:not(.e)');
+
+  var text = texts[pos];
+  for (var i = 0; i < texts.length; i++) {
+    texts[i].classList.remove('couleur');
+  }
+  text.classList.add('couleur');
+}
