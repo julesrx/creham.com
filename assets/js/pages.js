96b958ba6fc7581f172fb8bddbdea61c4713b9ed
julesrx <me@julesrx.fr>
2020-01-05 19:56:36 +0100

    cleanup

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,37 +1,41 @@
-var slider = document.getElementById('slider');
-var mask = slider.querySelector('.mask');
-
-var imgs = mask.querySelectorAll('img.slider-img');
+var slider = document.getElementById('slider'),
+  mask = slider.querySelector('.mask'),
+  imgs = getSliderImages(),
+  moving = 'moving';
 
 highlightText(0);
 
 setInterval(function () {
-  imgs = mask.querySelectorAll('img.slider-img');
+  var current = imgs[0],
+    after = imgs[1];
 
-  var current = imgs[0];
-  var after = imgs[1];
-
-  current.classList.add('moving');
-  after.classList.add('moving');
+  current.classList.add(moving);
+  after.classList.add(moving);
 
   setTimeout(function () {
     mask.append(current);
 
-    current.classList.remove('moving');
-    after.classList.remove('moving');
+    current.classList.remove(moving);
+    after.classList.remove(moving);
 
     var pos = Number(after.attributes['src'].value.match(/(?<=\/)\d(?=-)/g)[0]);
-    console.log(after.attributes['src'].value);
     highlightText(pos - 1);
-  }, 500);
+
+    imgs = getSliderImages();
+  }, 600);
 }, 3000);
 
 function highlightText(pos) {
   var texts = document.getElementsByClassName('slider-text')[0].querySelectorAll('span:not(.e)');
-
   var text = texts[pos];
+
   for (var i = 0; i < texts.length; i++) {
     texts[i].classList.remove('couleur');
   }
+
   text.classList.add('couleur');
+}
+
+function getSliderImages() {
+  return mask.querySelectorAll('img.slider-img');
 }
