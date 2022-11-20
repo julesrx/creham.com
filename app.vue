d088194d7c5fd784b9fc6d127ae076c258623e4f
julesrx <25800503+julesrx@users.noreply.github.com>
2022-11-20 12:23:30 +0100

    .

--- app.vue
+++ app.vue
@@ -1,5 +1,41 @@
+<script setup lang="ts">
+const config = useAppConfig();
+const { page } = useContent();
+</script>
+
 <template>
   <Html lang="fr" />
+
+  <Head>
+    <Meta charset="UTF-8" />
+    <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <Meta name="title" :content="config.title" />
+    <Meta name="description" :content="config.description" />
+    <Meta name="theme-color" :content="'#' + config.color" />
+
+    <!-- Twitter -->
+    <Meta name="twitter:card" content="summary" />
+    <Meta name="twitter:title" :content="config.title" />
+    <Meta name="twitter:description" :content="config.description" />
+    <Meta name="twitter:image" :content="config.url + '/img/thumbnail.png'" />
+
+    <!-- Open Graph / Facebook -->
+    <Meta property="og:title" :content="config.title" />
+    <Meta property="og:type" content="website" />
+    <Meta property="og:image" :content="config.url + '/img/thumbnail.png'" />
+    <Meta property="og:url" :content="config.url + page._path" />
+    <Meta property="og:description" :content="config.description" />
+    <Meta property="og:locale" content="fr_FR" />
+    <Meta property="og:email" :content="config.email" />
+
+    <link rel="canonical" :href="config.url" />
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
+
+    <Title v-if="page.title">{{ page.title }} - {{ config.title }}</Title>
+    <Title v-else>{{ config.short_description }}</Title>
+  </Head>
 
   <main id="container">
     <NuxtPage />
