197ac17b568962bc7e34f7ae8cc49c37cc2fc3d8
julesrx <25800503+julesrx@users.noreply.github.com>
2022-12-15 22:25:36 +0100

    Fix sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -2,12 +2,14 @@ import { serverQueryContent } from '#content/server';
 import { SitemapStream, streamToPromise } from 'sitemap';
 
 export default defineEventHandler(async event => {
-  // Fetch all documents
-  const docs = await serverQueryContent(event).find();
   const sitemap = new SitemapStream({ hostname: 'https://creham.com' });
 
+  sitemap.write({ url: '/', priority: 1.0 });
+
+  // Fetch all documents
+  const docs = await serverQueryContent(event).find();
   for (const doc of docs) {
-    sitemap.write({ url: doc._path });
+    sitemap.write({ url: doc._path, priority: 0.8 });
   }
 
   sitemap.end();
