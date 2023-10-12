445808b8e64b3de06bd60aff5cd6d2634159ca23
julesrx <25800503+julesrx@users.noreply.github.com>
2023-10-12 15:59:31 +0200

    up deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxt/image-edge'],
+  modules: ['@nuxt/content', '@nuxt/image'],
   css: ['@fontsource-variable/cabin', '/assets/css/main.scss'],
   content: { documentDriven: true },
   typescript: { typeCheck: true, strict: true },
