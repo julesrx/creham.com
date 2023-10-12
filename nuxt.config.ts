45559c52300e340a9fe19af0f7db80dbf417c760
julesrx <25800503+julesrx@users.noreply.github.com>
2023-10-12 15:53:46 +0200

    update font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/image-edge'],
-  css: ['@fontsource/cabin', '/assets/css/main.scss'],
+  css: ['@fontsource-variable/cabin', '/assets/css/main.scss'],
   content: { documentDriven: true },
   typescript: { typeCheck: true, strict: true },
   nitro: { prerender: { routes: ['/sitemap.xml'] } }
