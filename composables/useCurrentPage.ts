2921077950af01e85e0cd6826e2d1a55adadbb3b
Jules Raffoux <25800503+julesrx@users.noreply.github.com>
2024-09-27 21:35:33 +0200

    chore: update deps (#5)

--- composables/useCurrentPage.ts
+++ composables/useCurrentPage.ts
@@ -1,12 +1,15 @@
-import type { ParsedContent } from '@nuxt/content/dist/runtime/types';
+import type { ParsedContent } from '@nuxt/content';
 
 export default async function () {
   const route = useRoute();
 
-  const { data } = await useAsyncData<ParsedContent | undefined>('current-page', async () => {
-    if (route.path === '/') return undefined;
-    return await queryContent(route.path).findOne();
-  });
+  const { data } = await useAsyncData<ParsedContent | undefined>(
+    'current-page',
+    async () => {
+      if (route.path === '/') return undefined;
+      return await queryContent(route.path).findOne();
+    }
+  );
 
   return data;
 }
